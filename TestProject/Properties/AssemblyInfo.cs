015ddfa631c9505d6c8a2705b0d223670ce2523a
Иванов Максим Николаевич <m.ivanov@analit.net>
2020-02-21 00:33:54 +0300

    commit

--- TestProject/Properties/AssemblyInfo.cs
+++ TestProject/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestProject")]
+[assembly: AssemblyTitle("AddDataToPostresqlDB")]
 [assembly: AssemblyDescription("Adding and deleting data from PostgreSQL with MVVM pattern")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MaxIvanov")]
-[assembly: AssemblyProduct("TestProject")]
+[assembly: AssemblyProduct("AddDataToPostresqlDB")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
